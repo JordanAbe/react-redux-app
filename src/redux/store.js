50c5f45809dcea1741acea624c7b79f8a41cd572
Jordan Abe <j.abe@costamar.com>
2020-03-15 11:52:15 -0500

    redux hidratacion de estado, cuando se carga el componente pricipal se crea el evento unload para guardar el estado en localStorage ... cuando se cargue la pàgina redux inicia el estado con localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 import reducer from './reducers/index';
 
-const store = createStore(reducer);
+const globalState = localStorage.getItem('GLOBAL_STATE');
+const initialState = globalState ? JSON.parse(globalState) : undefined;
+const store = createStore(reducer, initialState);
+
+export const saveState = ()=> {
+    const state = store.getState();
+    localStorage.setItem('GLOBAL_STATE', JSON.stringify(state));
+}
 
 export default store;
